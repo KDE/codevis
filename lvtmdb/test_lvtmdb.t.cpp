9254372b37e9519ca7cdd42c2bff192a3e33e8e7
Tomaz Canabrava <tcanabrava@kde.org>
2023-07-27 15:12:03 +0200

    Add test file for lvtmdb
    Signed-off-by: Tomaz Canabrava <tcanabrava@kde.org>

---
+++ lvtmdb/test_lvtmdb.t.cpp
@@ -0,0 +1,95 @@
+// ct_lvtprj_project_file.t.cpp                                         -*-C++-*-
+
+/*
+// Copyright 2023 Codethink Ltd <codethink@codethink.co.uk>
+// SPDX-License-Identifier: Apache-2.0
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+*/
+
+#include <ct_lvtmdb_namespaceobject.h>
+#include <ct_lvtmdb_objectstore.h>
+#include <ct_lvtmdb_soci_reader.h>
+#include <ct_lvtmdb_soci_writer.h>
+
+#include <filesystem>
+#include <iostream>
+
+#include <QString> // for .endsWith
+
+#include <catch2/catch.hpp>
+
+namespace fs = std::filesystem;
+
+using namespace Codethink::lvtmdb;
+
+TEST_CASE("Merge Two Different Databases")
+{
+    ObjectStore store1;
+    ObjectStore store2;
+
+    NamespaceObject *store1_parent = nullptr;
+    NamespaceObject *store1_b = nullptr;
+    NamespaceObject *store1_c = nullptr;
+
+    NamespaceObject *store2_a = nullptr;
+    NamespaceObject *store2_parent = nullptr;
+    NamespaceObject *store2_b = nullptr;
+    NamespaceObject *store2_c = nullptr;
+
+    // Parent is the first one to be added, so it will probably have id 1
+    // on the first database.
+    store1.withRWLock([&] {
+        store1_parent = store1.getOrAddNamespace("parent", "parent", nullptr);
+        store1_b = store1.getOrAddNamespace("b", "b", store1_parent);
+        store1_c = store1.getOrAddNamespace("c", "c", store1_parent);
+    });
+
+    // parent is the second one to be added, so it will probably have id 2
+    // on the second database.
+    store2.withRWLock([&] {
+        store2_a = store2.getOrAddNamespace("a", "a", store1_parent);
+
+        // and that makes b and c from the store 2 point to a different id for parent,
+        // when the db is saved on disk.
+        store2_parent = store2.getOrAddNamespace("parent", "parent", nullptr);
+        store2_b = store2.getOrAddNamespace("b", "b", store1_parent);
+        store2_c = store2.getOrAddNamespace("c", "c", store1_parent);
+    });
+
+    fs::path project_path = fs::temp_directory_path();
+
+    std::error_code ec;
+    std::filesystem::remove(project_path / "database_1.db", ec);
+    std::filesystem::remove(project_path / "database_2.db", ec);
+    // We don't care about the error code.
+
+    SociWriter writer;
+    if (!writer.createOrOpen(project_path / "database_1.db")) {
+        std::cout << "Could not open the first test db" << std::endl;
+    }
+    store1.writeToDatabase(writer);
+
+    SociWriter writer2;
+    if (!writer2.createOrOpen("database_2.db")) {
+        std::cout << "Could not open the first test db" << std::endl;
+    }
+
+    SociReader reader;
+    ObjectStore mergedStore;
+    auto res = mergedStore.readFromDatabase(reader, project_path / "database_1.db");
+    CHECK(!res.has_error());
+
+    res = mergedStore.readFromDatabase(reader, project_path / "database_1.db");
+    CHECK(!res.has_error());
+}

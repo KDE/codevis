32377ba8e90bc4ba8d0b901b32e3db664841a02a
Tarcisio Fischer <tarcisio.fischer@codethink.co.uk>
2023-08-02 16:21:29 +0100

    Add optional comment parsing in logical parser
    Signed-off-by: Tarcisio Fischer <tarcisio.fischer@codethink.co.uk>

--- lvtclp/systemtests/project_with_includes_outside_src/prjA/groups/one/oneaaa/oneaaa_comp.h
+++ lvtclp/systemtests/project_with_includes_outside_src/prjA/groups/one/oneaaa/oneaaa_comp.h
@@ -1,11 +1,13 @@
 #ifndef DIAGRAM_SERVER_ONEAAA_COMP_H
 #define DIAGRAM_SERVER_ONEAAA_COMP_H
 
+// Test only include
 #include <twoaaa_comp.h>
 
+// @important klass
 class A {
   public:
     void foo() const;
 };
 
-#endif // DIAGRAM_SERVER_ONEAAA_COMP_H
+#endif

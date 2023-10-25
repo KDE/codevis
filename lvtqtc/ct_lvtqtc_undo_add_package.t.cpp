c6b1eae3c776a6558e76c8526191304cc208e203
Tomaz Canabrava <tcanabrava@kde.org>
2023-10-25 11:32:03 +0200

    Fix unittest

--- lvtqtc/ct_lvtqtc_undo_add_package.t.cpp
+++ lvtqtc/ct_lvtqtc_undo_add_package.t.cpp
@@ -44,8 +44,9 @@ TEST_CASE_METHOD(QTApplicationFixture, "Undo/Redo add component")
 
     // This GraphicsView is in the heap so that we can control it's lifetime
     auto *gv = new GraphicsViewWrapperForTesting{nodeStorage};
-    // TODO: Update this call.
-    // gv->updatePackageGraph(QString::fromStdString(a->qualifiedName()));
+    auto *scene = dynamic_cast<GraphicsScene *>(gv->scene());
+    scene->loadEntityByQualifiedName(QString::fromStdString(aa->qualifiedName()), QPoint(10, 10));
+
     gv->show();
 
     auto undoRedo = UndoManager{};

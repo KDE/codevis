d73d5aa146cd4213f7fa824a48ec759ed53d6be7
Tarcisio Fischer <tarcisio.fischer@codethink.co.uk>
2023-12-01 16:10:43 +0000

    Add packing for ubuntu-20.04
    Note: The scripts are not automated, and may get deprecated
    since they are untested. They were tested in the particular
    revision by the time of this commit.
    
    Signed-off-by: Tarcisio Fischer <tarcisio.fischer@codethink.co.uk>

---
+++ packaging/ubuntu-20.04/README.md
@@ -0,0 +1,18 @@
+Automated build scripts for Codevis
+---
+
+Run:
+```
+export CODEVIS_PKG_DIST=ubuntu-20.04
+export CODEVIS_PKG_OUTDIR=$(pwd)/codevis-build-${CODEVIS_PKG_DIST}/
+export CODEVIS_PKG_IMGNAME=codevis-${CODEVIS_PKG_DIST}
+cd ${CODEVIS_PKG_DIST}
+mkdir -p ${CODEVIS_PKG_OUTDIR}
+docker build . -t ${CODEVIS_PKG_IMGNAME}
+docker run -v ${CODEVIS_PKG_OUTDIR}:/artifacts -t ${CODEVIS_PKG_IMGNAME} 
+```
+
+Notes:
+- User will need to download KF5 packages using apt.
+- User will need to download LLVM17 package.
+- Tests will be disabled by default, and Ubuntu20.04 doesn't provide a Catch2 package on apt.
---
+++ packaging/ubuntu-20.04/deploy.sh
@@ -0,0 +1,40 @@
+# TODO: Review llvm .so version
+ln -s /usr/lib/llvm-17/lib/libclang-17.so.17 /usr/lib/llvm-17/lib/libclang-17.so.1
+
+# Ubuntu CMake is way too old, so we download a newer version
+wget https://github.com/Kitware/CMake/releases/download/v3.26.6/cmake-3.26.6-linux-x86_64.sh
+chmod +x cmake-3.26.6-linux-x86_64.sh
+mkdir -p /cmake/
+./cmake-3.26.6-linux-x86_64.sh --skip-license --prefix=/cmake/
+export PATH=/cmake/bin:$PATH
+
+git clone https://invent.kde.org/sdk/codevis.git
+cd codevis/ || exit
+mkdir -p build/ && cd build/ || exit
+cmake .. || exit
+cmake --build . -j"$(nproc)" || exit
+
+export CODEVIS_PKG_ARTIFACTS_PATH=/artifacts
+mkdir -p $CODEVIS_PKG_ARTIFACTS_PATH
+cp ./lvtqtd/liblvtqtd.so ${CODEVIS_PKG_ARTIFACTS_PATH}
+cp ./lvtcgn/liblvtcgn_gui.so ${CODEVIS_PKG_ARTIFACTS_PATH}
+cp ./lvtcgn/liblvtcgn_adapter.so ${CODEVIS_PKG_ARTIFACTS_PATH}
+cp ./lvtcgn/liblvtcgn_mdl.so ${CODEVIS_PKG_ARTIFACTS_PATH}
+cp ./lvtclp/liblvtclp.so ${CODEVIS_PKG_ARTIFACTS_PATH}
+cp ./lvtmdl/liblvtmdl.so ${CODEVIS_PKG_ARTIFACTS_PATH}
+cp ./lvtshr/liblvtshr.so ${CODEVIS_PKG_ARTIFACTS_PATH}
+cp ./lvtmdb/liblvtmdb.so ${CODEVIS_PKG_ARTIFACTS_PATH}
+cp ./lvtqtw/liblvtqtw.so ${CODEVIS_PKG_ARTIFACTS_PATH}
+cp ./lvtprj/liblvtprj.so ${CODEVIS_PKG_ARTIFACTS_PATH}
+cp ./lvtqtc/liblvtqtc.so ${CODEVIS_PKG_ARTIFACTS_PATH}
+cp ./lvtclr/liblvtclr.so ${CODEVIS_PKG_ARTIFACTS_PATH}
+cp ./lvtplg/liblvtplg.so ${CODEVIS_PKG_ARTIFACTS_PATH}
+cp ./lvtldr/liblvtldr.so ${CODEVIS_PKG_ARTIFACTS_PATH}
+cp ./liblakospreferences.so ${CODEVIS_PKG_ARTIFACTS_PATH}
+cp ./thirdparty/MRichTextEditor/libMRichTextEdit.so ${CODEVIS_PKG_ARTIFACTS_PATH}
+cp ./lib/libsoci_core.so.4.1 ${CODEVIS_PKG_ARTIFACTS_PATH}
+cp ./lib/libsoci_sqlite3.so.4.1 ${CODEVIS_PKG_ARTIFACTS_PATH}
+cp ./desktopapp/codevis_desktop ${CODEVIS_PKG_ARTIFACTS_PATH}
+cp ./lvtclp/codevis_create_codebase_db ${CODEVIS_PKG_ARTIFACTS_PATH}
+cp ./lvtclp/codevis_merge_databases ${CODEVIS_PKG_ARTIFACTS_PATH}
+cp ./lvtprj/codevis_create_prj_from_db ${CODEVIS_PKG_ARTIFACTS_PATH}

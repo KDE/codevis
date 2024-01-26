270b151ba75629328f8f97bcc11aade9f0d6b10e
Tomaz Canabrava <tcanabrava@kde.org>
2024-01-26 09:43:34 +0100

    Fix installing python plugins

--- cmake/CodevisDeployPlugin.cmake
+++ cmake/CodevisDeployPlugin.cmake
@@ -4,5 +4,5 @@ macro (codevis_install_plugin PLUGIN_NAME)
 endmacro()
 
 macro (codevis_install_python_plugin PLUGIN_NAME)
-    install(FILES ${PLUGIN_NAME} README.md metadata.json DESTINATION "${KDE_INSTALL_DATAROOTDIR}/codevis/plugins/${PLUGIN_NAME}")
+    install(FILES ${PLUGIN_NAME}.py README.md metadata.json DESTINATION "${KDE_INSTALL_DATAROOTDIR}/codevis/plugins/${PLUGIN_NAME}")
 endmacro()

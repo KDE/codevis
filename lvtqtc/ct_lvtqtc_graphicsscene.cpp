f665789a639cdf6a7371700ad48fca2b77be5b29
Tomaz Canabrava <tcanabrava@kde.org>
2024-02-27 18:59:04 +0100

    Properly fix loading of Bookmarks

--- lvtqtc/ct_lvtqtc_graphicsscene.cpp
+++ lvtqtc/ct_lvtqtc_graphicsscene.cpp
@@ -1773,9 +1773,6 @@ void recursiveJsonToLakosEntity(GraphicsScene *scene, const QJsonValue& entity)
 
 void GraphicsScene::fromJson(const QJsonObject& doc)
 {
-    QElapsedTimer timer;
-    timer.start();
-
     // Hack. if we add elements to the GraphiscScene, it will
     // trigger a repaint, and we might be adding *thousands* of edges
     // here. because edges have a big boundingRect, this will trigger
@@ -1820,29 +1817,28 @@ void GraphicsScene::fromJson(const QJsonObject& doc)
 
     views().at(0)->viewport()->setVisible(true);
 
-    {
-        // Do not remove this, this is a hack to fix the edges pointing to nowhere.
-        // Or Better, Remove this when this hack is not needed anymore.
-        // it does not really matter that the strings are slightly incorrect,
-        // but they allow us to not show garbage to the user, and that matters more.
-        // The error appears to be that the initial setup contains bogus information
-        // about the viewport because we might position things outside of the view
-        // area.
-        // the Event Loop is not helping because it calculates things while the view
-        // is not visible yet, so I need to halt this method using something that
-        // awaits for user interaction. when the user *sees* this, it means that the
-        // view is ok, and I can continue with the collapse / expand.
-        // the relayout() call below is currently needed but might be easier to remove
-        // than the start of this hack.
-        // not really proud of this code, but, hey, it works.
-
-        QMessageBox::about(views().at(0), tr("Loading finished"), tr("Bookmark load took %1 ms").arg(timer.elapsed()));
+    // Do not remove this, this is a hack to fix the edges pointing to nowhere.
+    // Or Better, Remove this when this hack is not needed anymore.
+    // it does not really matter that the strings are slightly incorrect,
+    // but they allow us to not show garbage to the user, and that matters more.
+    // The error appears to be that the initial setup contains bogus information
+    // about the viewport because we might position things outside of the view
+    // area.
+    // the Event Loop is not helping because it calculates things while the view
+    // is not visible yet, so I need to halt this method using something that
+    // awaits for user interaction. when the user *sees* this, it means that the
+    // view is ok, and I can continue with the collapse / expand.
+    // the relayout() call below is currently needed but might be easier to remove
+    // than the start of this hack.
+    // not really proud of this code, but, hey, it works.
+    QTimer::singleShot(std::chrono::seconds{1}, [this] {
         collapseToplevelEntities();
         expandToplevelEntities();
 
-        QMessageBox::about(views().at(0), tr("Loading finished"), tr("Relayout took %1 ms").arg(timer.elapsed()));
-        reLayout();
-    }
+        QTimer::singleShot(std::chrono::seconds{1}, [this] {
+            reLayout();
+        });
+    });
 }
 
 void GraphicsScene::setPluginManager(Codethink::lvtplg::PluginManager& pm)

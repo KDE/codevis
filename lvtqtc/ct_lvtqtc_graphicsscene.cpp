13b4c2f8b4799c9839ef1ff9c7aaa8ce26f873d6
Tomaz Canabrava <tcanabrava@kde.org>
2024-02-27 18:13:11 +0100

    Speed up by 7x loading elements from Bookmarks

--- lvtqtc/ct_lvtqtc_graphicsscene.cpp
+++ lvtqtc/ct_lvtqtc_graphicsscene.cpp
@@ -61,11 +61,13 @@
 #include <QAction>
 #include <QApplication>
 #include <QCursor>
+#include <QElapsedTimer>
 #include <QGraphicsRectItem>
 #include <QGraphicsSceneContextMenuEvent>
 #include <QGraphicsView>
 #include <QLoggingCategory>
 #include <QMenu>
+#include <QMessageBox>
 #include <QScreen>
 #include <QTextBrowser>
 #include <QTimer>
@@ -1771,6 +1773,20 @@ void recursiveJsonToLakosEntity(GraphicsScene *scene, const QJsonValue& entity)
 
 void GraphicsScene::fromJson(const QJsonObject& doc)
 {
+    QElapsedTimer timer;
+    timer.start();
+
+    // Hack. if we add elements to the GraphiscScene, it will
+    // trigger a repaint, and we might be adding *thousands* of edges
+    // here. because edges have a big boundingRect, this will trigger
+    // a repaint and cache invalidation of the view *thousands* of times.
+    // making loading huge graphs quite slow.
+    // so we hide the viewport, it will not try to paint anything untill
+    // we set it to visible again.
+    // before this change: Load From Jason Took: 1778ms
+    // after this change: Load from Json Took: 230ms
+    views().at(0)->viewport()->setVisible(false);
+
     Q_EMIT graphLoadStarted();
     clearGraph();
 
@@ -1801,6 +1817,32 @@ void GraphicsScene::fromJson(const QJsonObject& doc)
     const auto show_transitive = doc["transitive_visibility"].toBool();
     toggleTransitiveRelationVisibility(show_transitive);
     Q_EMIT graphLoadFinished();
+
+    views().at(0)->viewport()->setVisible(true);
+
+    {
+        // Do not remove this, this is a hack to fix the edges pointing to nowhere.
+        // Or Better, Remove this when this hack is not needed anymore.
+        // it does not really matter that the strings are slightly incorrect,
+        // but they allow us to not show garbage to the user, and that matters more.
+        // The error appears to be that the initial setup contains bogus information
+        // about the viewport because we might position things outside of the view
+        // area.
+        // the Event Loop is not helping because it calculates things while the view
+        // is not visible yet, so I need to halt this method using something that
+        // awaits for user interaction. when the user *sees* this, it means that the
+        // view is ok, and I can continue with the collapse / expand.
+        // the relayout() call below is currently needed but might be easier to remove
+        // than the start of this hack.
+        // not really proud of this code, but, hey, it works.
+
+        QMessageBox::about(views().at(0), tr("Loading finished"), tr("Bookmark load took %1 ms").arg(timer.elapsed()));
+        collapseToplevelEntities();
+        expandToplevelEntities();
+
+        QMessageBox::about(views().at(0), tr("Loading finished"), tr("Relayout took %1 ms").arg(timer.elapsed()));
+        reLayout();
+    }
 }
 
 void GraphicsScene::setPluginManager(Codethink::lvtplg::PluginManager& pm)

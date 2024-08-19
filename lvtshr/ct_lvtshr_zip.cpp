2a4847079fa114a08606cb2f032ea264072478a7
Tomaz Canabrava <tcanabrava@kde.org>
2024-08-19 10:39:31 +0200

    Port the uncompress feature from KArchive to libzip
    I do not want to deal with politics right now, so, moving to a worse
    api.

--- lvtshr/ct_lvtshr_zip.cpp
+++ lvtshr/ct_lvtshr_zip.cpp
@@ -5,6 +5,8 @@
 #include <QFileInfo>
 
 #include <KZip>
+#include <filesystem>
+#include <zip.h>
 
 using namespace Codethink::lvtshr;
 
@@ -31,17 +33,66 @@ cpp::result<void, ZipError> Zip::compressFolder(QDir& folder, const QFileInfo& z
 
 cpp::result<void, ZipError> Zip::uncompressToFolder(QDir& folder, const QFileInfo& zipFileName)
 {
-    auto zipFile = KZip(zipFileName.absoluteFilePath());
-    if (!zipFile.open(QIODevice::ReadOnly)) {
-        return cpp::fail(std::string("Could not open file to read contents:")
-                         + zipFileName.absoluteFilePath().toStdString() + "  " + zipFile.errorString().toStdString());
+    // Conversion dance.
+    QByteArray archiveNameArray = zipFileName.absoluteFilePath().toLocal8Bit();
+    const char *archive_name = archiveNameArray.constData();
+
+    int err = 0;
+    struct zip *zip_archive = zip_open(archive_name, 0, &err);
+    if (zip_archive == nullptr) {
+        char buf[100];
+        zip_error_to_str(buf, sizeof(buf), err, errno);
+        return cpp::fail(ZipError{buf});
     }
 
-    const KArchiveDirectory *dir = zipFile.directory();
-    bool copied = dir->copyTo(folder.absolutePath());
-    if (!copied) {
-        return cpp::fail(std::string("Error copying files to directory") + zipFileName.absoluteFilePath().toStdString()
-                         + "  " + zipFile.errorString().toStdString());
+    for (int i = 0; i < zip_get_num_entries(zip_archive, 0); i++) {
+        struct zip_stat st;
+        zip_stat_init(&st);
+
+        int err = zip_stat_index(zip_archive, i, 0, &st);
+        if (err < 0) {
+            return cpp::fail(ZipError{"Error stating the file"});
+        }
+
+        QString fileName = st.name;
+        // If it's a directory, create it and run the next loop.'
+        if (fileName.endsWith('/')) {
+            QString dirName = folder.absolutePath() + "/" + fileName;
+            std::filesystem::create_directories(dirName.toStdString());
+            continue;
+        }
+
+        // Open the zip data, and the file in disk.
+        struct zip_file *zf = zip_fopen_index(zip_archive, i, 0);
+        if (!zf) {
+            return cpp::fail(ZipError{"Failed to read zip archive"});
+        }
+
+        QFile localFile(folder.absolutePath() + "/" + fileName);
+        if (!localFile.open(QIODevice::WriteOnly)) {
+            return cpp::fail(ZipError{localFile.errorString().toStdString()});
+        }
+
+        // Save the zip data on the local disk file.
+        // Alloc memory for its uncompressed contents
+        std::vector<char> contents(st.size);
+        if (zip_fread(zf, contents.data(), st.size) == -1) {
+            return cpp::fail(ZipError{"Error reading file contents from zip file"});
+        }
+
+        if (localFile.write(contents.data(), contents.size()) == -1) {
+            return cpp::fail(ZipError{localFile.errorString().toStdString()});
+        }
+
+        err = zip_fclose(zf);
+        if (err != 0) {
+            return cpp::fail(ZipError{"Failed to close file buffer from zip file"});
+        }
+    }
+
+    err = zip_close(zip_archive);
+    if (err != 0) {
+        return cpp::fail(ZipError{"Failed to close zip file"});
     }
 
     return {};

4ba1e93336e10e75329769773e95ac5c43c6f6f2
Tomaz Canabrava <tcanabrava@kde.org>
2024-08-20 11:10:32 +0200

    Use moc_* includes on .cpp for the plugin
    Should fix the build on windows.

--- plugin_system/ICodevisPlugin.cpp
+++ plugin_system/ICodevisPlugin.cpp
@@ -1,9 +1,12 @@
 #include <ICodevisPlugin.h>
 
-using namespace Codevis::PluginSystem;
+namespace Codevis::PluginSystem {
 
 ICodevisPlugin::ICodevisPlugin(QObject *parent, const QVariantList& args)
 {
     std::ignore = parent;
     std::ignore = args;
 }
+
+} // namespace Codevis::PluginSystem
+#include "moc_ICodevisPlugin.cpp"
